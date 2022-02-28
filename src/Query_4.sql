aa496689389050c5ce33530621c00516fc967646
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-28 14:33:44 +0530

    Write query to place a new order

---
+++ src/Query_4.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Products`(`product_id`, `product_name`, `product_image`, `product_category`, `product_sale_price`, `product_list_price`) VALUES (200,'Mobile',"mobile.png","electronics",13000,15000);
+
+INSERT INTO `Orders`(`order_id`, `user_id`, `status`, `total_amount`, `shipping_address`, `shipping_pincode`, `order_date`, `delivery_date`) VALUES (300,101,"pending",13000,'Nagpur', 440001 ,'2022-02-28', '2022-03-07');
+
+INSERT INTO `Order_items`(`order_id`, `product_id`, `discount`, `quantity`) VALUES (300,200,'20 per','2');
