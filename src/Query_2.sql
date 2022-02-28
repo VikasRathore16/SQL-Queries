cb881fc9c7d099600c807fd28887d5e4cdab2a68
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-28 13:29:03 +0530

    Write query to add 5 users

---
+++ src/Query_2.sql
@@ -0,0 +1,9 @@
+INSERT INTO `Users`(`user_id`, `username`, `password`, `firstname`, `lastname`, `email`, `role`, `address`, `pincode`) VALUES (100,"Vikas16","vikas@123","Vikas","Rathore","vikasrathore@cedcoss.com","customer","jankipuram",440001);
+
+INSERT INTO `Users`(`user_id`, `username`, `password`, `firstname`, `lastname`, `email`, `role`, `address`, `pincode`) VALUES (101,"Rohit12","Rohit@123","Rohit","Rathore","rohitrathore@cedcoss.com","customer","jankipuram",440001);
+
+INSERT INTO `Users`(`user_id`, `username`, `password`, `firstname`, `lastname`, `email`, `role`, `address`, `pincode`) VALUES (103,"Ritick10","Ritick@123","Ritick","Rai","ritickrai@cedcoss.com","customer","jankipuram",440001);
+
+INSERT INTO `Users`(`user_id`, `username`, `password`, `firstname`, `lastname`, `email`, `role`, `address`, `pincode`) VALUES (104,"Mohit21","Mohit@123","Mohit","Rathore","mohitrathore@cedcoss.com","admin","jankipuram",110001);
+
+INSERT INTO `Users`(`user_id`, `username`, `password`, `firstname`, `lastname`, `email`, `role`, `address`, `pincode`) VALUES (105,"Neha21","Neha@123","Neha","Rathore","Neharathore@cedcoss.com","manager","jankipuram",110001);
