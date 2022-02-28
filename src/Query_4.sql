25804db76849945b5bbeb70928d1eea3ae1250a8
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-28 14:39:03 +0530

    Write query to place a new order

--- src/Query_4.sql
+++ src/Query_4.sql
@@ -2,4 +2,7 @@ INSERT INTO `Products`(`product_id`, `product_name`, `product_image`, `product_c
 
 INSERT INTO `Orders`(`order_id`, `user_id`, `status`, `total_amount`, `shipping_address`, `shipping_pincode`, `order_date`, `delivery_date`) VALUES (300,101,"pending",13000,'Nagpur', 440001 ,'2022-02-28', '2022-03-07');
 
-INSERT INTO `Order_items`(`order_id`, `product_id`, `discount`, `quantity`) VALUES (300,200,'20 per','2');
+INSERT INTO `Orders`(`order_id`, `user_id`, `status`, `total_amount`, `shipping_address`, `shipping_pincode`, `order_date`, `delivery_date`) VALUES (301,104,"pending",13000,'Delhi', 110001 ,'2022-02-28', '2022-03-07');
+
+INSERT INTO `Order_items`(`order_id`, `product_id`, `discount`, `quantity`) VALUES (300,200,'20 percentage','2');
+INSERT INTO `Order_items`(`order_id`, `product_id`, `discount`, `quantity`) VALUES (301,200,'20 percentage','2');
